c9856ab2637d48325db050a689959a8cb2239193
Thierry61 <tfavier61@gmail.com>
2023-06-25 18:41:21 +0200

    Move static asset (btc.svg) to public directory

--- app/header.js
+++ app/header.js
@@ -1,11 +1,10 @@
 import Image from 'next/image'
-import btc from './btc.svg'
 
 export default async function Header() {
   return (
     <header className='flex flex-cols justify-between gap-2 m-3'>
       <div>
-        <Image className='inline mr-2 mb-1' src={btc} width={24} height={24} alt="Bitcoin logo"/>
+        <Image className='inline mr-2 mb-1' src='/btc.svg' width={24} height={24} alt="Bitcoin logo"/>
         <span>Node Summary</span>
       </div>
     </header>
