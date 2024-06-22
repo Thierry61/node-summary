173b3487ba7e643effd1168dfdaddd75be3faa70
Thierry61 <tfavier61@gmail.com>
2024-06-22 21:07:36 +0200

    Updated dependencies:
    - "npx npm-check-updates -u"
    - npm install
    - put back 8.56.6 for eslint because 9.5.0 is not compatible with eslint-config-next 14.2.4
    - "npm audit fix" to address high severity vulnerability on braces
    
    Then I came across error : Dynamic server usage: no-store fetch http://127.0.0.1:8332
    when building the site.
    
    So I created a new project from scratch with "npx create-next-app@latest"
    and copied dependencies from generated package.json file.
    
    Note that
    - some dependencies become dev dependencies
    - autoprefixer dependency disappears
    - postcss.config.js is renamed postcss.config.mjs,  autoprefixer plugin is removed from it and is exported differently
    - next.config.js is renamed next.config.mjs and is exported differently
    
    But the error was still appearing with "npm run build" (but site was functional).
    
    This was corrected by adding export const dynamic = "force-dynamic";
    in page.js. But I don't know the consequences of this.

--- app/page.js
+++ app/page.js
@@ -1,4 +1,9 @@
 // Server component reloaded periodically
+
+// Force dynamic to avoir error: Dynamic server usage: no-store fetch http://127.0.0.1:8332
+// when building the site
+export const dynamic = "force-dynamic";
+
 import PeriodicComponent from './periodic-component'
 
 export default async function Home() {
