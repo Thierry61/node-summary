4466417c8b10dd9957e59534ac05a4cbff6b2d4d
Thierry61 <tfavier61@gmail.com>
2024-07-12 16:35:00 +0200

    Display node subversion instead of RPC URL

--- app/header.js
+++ app/header.js
@@ -1,11 +1,15 @@
 import Image from 'next/image'
+import { getVersion } from '../lib/load-summary'
 
 export default async function Header() {
+  const vers = await getVersion()
+  const err = vers.err
+  const vers_or_err = err == undefined ? vers.subversion : JSON.stringify(err)
   return (
     <header className='sticky top-0 z-30 w-full bg-white flex flex-cols justify-between'>
       <div className='m-2'>
         <Image className='inline mr-2' src='/btc.svg' width={24} height={24} alt="Bitcoin logo"/>
-        <span className='inline align-middle'>Node Summary - {process.env.BITCOIND_HOST}:{process.env.BITCOIND_PORT}</span>
+        <span className='inline align-middle'>Node Summary - {vers_or_err}</span>
       </div>
     </header>
   )
